97c465f49914205612cb73d5d4555d511f654d55
Wolf McNally <wolf@wolfmcnally.com>
2017-10-24 03:55:59 -0700

    Various.

--- WolfBase/Sources/Geometry/Insets.swift
+++ WolfBase/Sources/Geometry/Insets.swift
@@ -7,17 +7,21 @@
 //
 
 public struct Insets {
-  public var top: Double?
-  public var left: Double?
-  public var bottom: Double?
-  public var right: Double?
-  
-  public init(top: Double? = nil, left: Double? = nil, bottom: Double? = nil, right: Double? = nil) {
-    self.top = top
-    self.left = left
-    self.bottom = bottom
-    self.right = right
-  }
-  
-  public static let zero = Insets(top: 0, left: 0, bottom: 0, right: 0)
+    public var top: Double?
+    public var left: Double?
+    public var bottom: Double?
+    public var right: Double?
+    
+    public init(top: Double? = nil, left: Double? = nil, bottom: Double? = nil, right: Double? = nil) {
+        self.top = top
+        self.left = left
+        self.bottom = bottom
+        self.right = right
+    }
+    
+    public init(size: Double) {
+        self.init(top: size, left: size, bottom: size, right: size)
+    }
+    
+    public static let zero = Insets(top: 0, left: 0, bottom: 0, right: 0)
 }
--- WolfBase/Sources/Persistence/UserDefaultsExtensions.swift
+++ WolfBase/Sources/Persistence/UserDefaultsExtensions.swift
@@ -14,24 +14,25 @@ extension LogGroup {
   public static let userDefaults = LogGroup("userDefaults")
 }
 
-//extension UserDefaults {
-//  public subscript(key: String) -> Any? {
-//    get {
-//      let value = userDefaults.object(forKey: key)
-//      logTrace("get key: \(key), value: \(value†)", group: .userDefaults)
-//      return value as AnyObject?
-//    }
-//    set {
-//      logTrace("set key: \(key), newValue: \(newValue†)", group: .userDefaults)
-//      if let newValue = newValue {
-//        userDefaults.set(newValue, forKey: key)
-//      } else {
-//        userDefaults.removeObject(forKey: key)
-//      }
-//      userDefaults.synchronize()
-//    }
-//  }
-//}
+extension UserDefaults {
+  public subscript(key: String) -> Any? {
+    get {
+      let value = userDefaults.object(forKey: key)
+      logTrace("get key: \(key), value: \(value†)", group: .userDefaults)
+      return value as AnyObject?
+    }
+    set {
+      logTrace("set key: \(key), newValue: \(newValue†)", group: .userDefaults)
+      if let newValue = newValue {
+        userDefaults.set(newValue, forKey: key)
+      } else {
+        userDefaults.removeObject(forKey: key)
+      }
+      userDefaults.synchronize()
+    }
+  }
+}
+
 
 // Based on "Type-Safe User Defaults" by Mike Ash
 //
