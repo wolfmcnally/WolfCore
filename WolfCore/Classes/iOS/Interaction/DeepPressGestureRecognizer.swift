3de35a5e32af8121eccdc678253c1090171beda4
Wolf McNally <wolf@wolfmcnally.com>
2018-02-11 17:47:16 -0800

    Split out FeedbackGenerator into it's own file. Fixed responsiveness of DeepPressGestureRecognizer.

--- WolfCore/Classes/iOS/Interaction/DeepPressGestureRecognizer.swift
+++ WolfCore/Classes/iOS/Interaction/DeepPressGestureRecognizer.swift
@@ -6,118 +6,60 @@
 //  Copyright © 2018 WolfMcNally.com. All rights reserved.
 //
 
-//
-// Based on:
-// https://github.com/FlexMonkey/DeepPressGestureRecognizer/
-//
-
-//
-//  DeepPressGestureRecognizer.swift
-//  DeepPressGestureRecognizer
-//
-//  Created by SIMON_NON_ADMIN on 03/10/2015.
-//  Copyright © 2015 Simon Gladman. All rights reserved.
-//
-//  Thanks to Alaric Cole - bridging header replaced by proper import :)
-import AudioToolbox
 import UIKit
 import UIKit.UIGestureRecognizerSubclass
-
-public let feedbackGenerator = FeedbackGenerator()
-
-public class FeedbackGenerator {
-    //available(iOS, 10.0, *)
-    private var selectionGenerator: Any?
-    private var heavyGenerator: Any?
-    private var mediumGenerator: Any?
-    private var lightGenerator: Any?
-    private var notificationGenerator: Any?
-
-    public func selectionChanged() {
-        if #available(iOS 10.0, *) {
-            if selectionGenerator == nil {
-                selectionGenerator = UISelectionFeedbackGenerator()
-            }
-
-            (selectionGenerator as! UISelectionFeedbackGenerator).selectionChanged()
-        }
-    }
-
-    public func heavy() {
-        if #available(iOS 10.0, *) {
-            if heavyGenerator == nil {
-                heavyGenerator = UIImpactFeedbackGenerator(style: .heavy)
-            }
-
-            (heavyGenerator as! UIImpactFeedbackGenerator).impactOccurred()
-        }
-    }
-
-    public func medium() {
-        if #available(iOS 10.0, *) {
-            if mediumGenerator == nil {
-                mediumGenerator = UIImpactFeedbackGenerator(style: .medium)
-            }
-
-            (mediumGenerator as! UIImpactFeedbackGenerator).impactOccurred()
-        }
-    }
-
-    public func light() {
-        if #available(iOS 10.0, *) {
-            if lightGenerator == nil {
-                lightGenerator = UIImpactFeedbackGenerator(style: .light)
-            }
-
-            (lightGenerator as! UIImpactFeedbackGenerator).impactOccurred()
-        }
-    }
-
-    public func error() {
-        if #available(iOS 10.0, *) {
-            if notificationGenerator == nil {
-                notificationGenerator = UINotificationFeedbackGenerator()
-            }
-
-            (notificationGenerator as! UINotificationFeedbackGenerator).notificationOccurred(.error)
-        }
-    }
-
-    public func success() {
-        if #available(iOS 10.0, *) {
-            if notificationGenerator == nil {
-                notificationGenerator = UINotificationFeedbackGenerator()
-            }
-
-            (notificationGenerator as! UINotificationFeedbackGenerator).notificationOccurred(.success)
-        }
-    }
-
-    public func warning() {
-        if #available(iOS 10.0, *) {
-            if notificationGenerator == nil {
-                notificationGenerator = UINotificationFeedbackGenerator()
-            }
-
-            (notificationGenerator as! UINotificationFeedbackGenerator).notificationOccurred(.warning)
-        }
-    }
-}
 
 // MARK: GestureRecognizer
 public class DeepPressGestureRecognizer: UIGestureRecognizer
 {
     public var forceThreshold: Frac = 0.75
-    public var timeThreshold: TimeInterval = 0
+    public var timeThreshold: TimeInterval = 0 {
+        didSet {
+            syncToTimeThreshold()
+        }
+    }
+
+    private var needsBeginGesture: Asynchronizer?
 
     enum State {
         case notPressing
         case pressingLight
-        case pressingHeavy(TimeInterval) // startTime
-        case pressingDeep
+        case pressedHeavy
+        case pressedDeep
     }
 
-    private var myState: State = .notPressing
+    private func syncToTimeThreshold() {
+        needsBeginGesture = Asynchronizer(delay: timeThreshold) { [unowned self] in
+            self.beginGesture()
+        }
+    }
+
+    private func beginGesture() {
+        myState = .pressedDeep
+        state = .began
+        feedbackGenerator.heavy()
+    }
+
+    private func setNeedsBeginGesture() {
+        myState = .pressedHeavy
+        if let needsBeginGesture = needsBeginGesture {
+            feedbackGenerator.light()
+            needsBeginGesture.setNeedsSync()
+        } else {
+            beginGesture()
+        }
+    }
+
+    private func cancelBeginGesture() {
+        myState = .notPressing
+        needsBeginGesture?.cancel()
+    }
+
+    private var myState: State = .notPressing {
+        didSet {
+            //print(myState)
+        }
+    }
 
     public required init(target: AnyObject? = nil, action: Selector? = nil) {
         super.init(target: target, action: action)
@@ -139,19 +81,19 @@ public class DeepPressGestureRecognizer: UIGestureRecognizer
         super.touchesEnded(touches, with: event)
 
         switch myState {
-        case .pressingDeep:
+        case .pressedDeep:
             state = .ended
         default:
             state = .failed
         }
-        myState = .notPressing
+        cancelBeginGesture()
     }
 
     public override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent) {
         super.touchesCancelled(touches, with: event)
 
         state = .ended
-        myState = .notPressing
+        cancelBeginGesture()
     }
 
     private func handleTouch(_ touch: UITouch) {
@@ -165,22 +107,11 @@ public class DeepPressGestureRecognizer: UIGestureRecognizer
             myState = .pressingLight
         case .pressingLight:
             if force >= forceThreshold {
-                myState = .pressingHeavy(Date.timeIntervalSinceReferenceDate)
-                if timeThreshold > 0 {
-                    feedbackGenerator.light()
-                }
+                setNeedsBeginGesture()
             }
-        case .pressingHeavy(let startTime):
-            if force >= forceThreshold {
-                if Date.timeIntervalSinceReferenceDate - startTime > timeThreshold {
-                    myState = .pressingDeep
-                    state = .began
-                    feedbackGenerator.heavy()
-                }
-            } else {
-                myState = .pressingLight
-            }
-        case .pressingDeep:
+        case .pressedHeavy:
+            break
+        case .pressedDeep:
             break
         }
     }
