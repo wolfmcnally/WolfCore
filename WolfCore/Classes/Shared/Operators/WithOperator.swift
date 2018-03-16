4e9e63304283717e592731662329e345af17acd3
Wolf McNally <wolf@wolfmcnally.com>
2018-03-16 03:31:32 -0700

    Added Queue type. Enhanced with-operator.

--- WolfCore/Classes/Shared/Operators/WithOperator.swift
+++ WolfCore/Classes/Shared/Operators/WithOperator.swift
@@ -13,6 +13,12 @@
 ///
 infix operator • : CastingPrecedence
 infix operator •• : CastingPrecedence
+infix operator ••• : CastingPrecedence
+
+@discardableResult public func • <T: AnyObject>(lhs: T, rhs: (T) -> Void) -> T {
+    rhs(lhs)
+    return lhs
+}
 
 @discardableResult public func •• <T: Any>(lhs: T, rhs: (inout T) -> Void) -> T {
     var lhs = lhs
@@ -20,7 +26,6 @@ infix operator •• : CastingPrecedence
     return lhs
 }
 
-@discardableResult public func • <T: AnyObject>(lhs: T, rhs: (T) -> Void) -> T {
-    rhs(lhs)
-    return lhs
+public func ••• <T: Any>(lhs: inout T, rhs: (inout T) -> Void) {
+    rhs(&lhs)
 }
