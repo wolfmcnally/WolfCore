afbb5b9949578ccf830fd6c1ee0fd6e9fc9cdedc
Wolf McNally <wolf@wolfmcnally.com>
2018-04-10 14:23:12 -0700

    Removed CodableUtils. Nice idea, but the lack of throwing subscripts makes it not worth it.

--- WolfCore/Classes/Shared/DataFormats/CodableUtils.swift
+++ WolfCore/Classes/Shared/DataFormats/CodableUtils.swift
@@ -8,35 +8,57 @@
 
 import Foundation
 
-extension Decoder {
-    public subscript<Key>(type: Key.Type) -> KeyedDecodingContainer<Key> where Key: CodingKey {
-        return try! self.container(keyedBy: type)
-    }
-}
+//extension Decoder {
+//    public static func <| <Key>(lhs: Self, rhs: Key.Type) throws -> KeyedDecodingContainer<Key> where Key: CodingKey {
+//        return try lhs.container(keyedBy: rhs)
+//    }
+//}
 
-extension Encoder {
-    public subscript<Key>(type: Key.Type) -> KeyedEncodingContainer<Key> where Key: CodingKey {
-        return self.container(keyedBy: type)
-    }
-}
-
-extension KeyedDecodingContainer {
-    public subscript<T>(key: Key) -> T? where T: Decodable {
-        guard contains(key) else { return nil }
-        return try! decode(T.self, forKey: key)
-    }
-
-    public subscript<NestedKey>(type: NestedKey.Type, key: Key) -> KeyedDecodingContainer<NestedKey> {
-        return try! nestedContainer(keyedBy: type, forKey: key)
-    }
-}
-
-extension KeyedEncodingContainer {
-    public subscript<T>(key: Key) -> T? where T: Encodable {
-        get { fatalError("Cannot read from an encoding container") }
-        set {
-            guard let newValue = newValue else { return }
-            try! encode(newValue, forKey: key)
-        }
-    }
-}
+//extension Decoder {
+//    public subscript<Key>(type: Key.Type) -> KeyedDecodingContainer<Key> where Key: CodingKey {
+//        do {
+//            return try self.container(keyedBy: type)
+//        } catch {
+//            fatalError(error.localizedDescription)
+//        }
+//    }
+//}
+//
+//extension Encoder {
+//    public subscript<Key>(type: Key.Type) -> KeyedEncodingContainer<Key> where Key: CodingKey {
+//        return self.container(keyedBy: type)
+//    }
+//}
+//
+//extension KeyedDecodingContainer {
+//    public subscript<T>(key: Key) -> T? where T: Decodable {
+//        guard contains(key) else { return nil }
+//        do {
+//            return try decode(T.self, forKey: key)
+//        } catch {
+//            fatalError(error.localizedDescription)
+//        }
+//    }
+//
+//    public subscript<NestedKey>(type: NestedKey.Type, key: Key) -> KeyedDecodingContainer<NestedKey> {
+//        do {
+//            return try nestedContainer(keyedBy: type, forKey: key)
+//        } catch {
+//            fatalError(error.localizedDescription)
+//        }
+//    }
+//}
+//
+//extension KeyedEncodingContainer {
+//    public subscript<T>(key: Key) -> T? where T: Encodable {
+//        get { fatalError("Cannot read from an encoding container") }
+//        set {
+//            guard let newValue = newValue else { return }
+//            do {
+//                try encode(newValue, forKey: key)
+//            } catch {
+//                fatalError(error.localizedDescription)
+//            }
+//        }
+//    }
+//}
