26aa6cfa1014882f488ada40ae5910cf9b6844d0
Wolf McNally <wolf@wolfmcnally.com>
2019-04-07 17:41:58 -0700

    Fixed support for SPM.

--- Package.swift
+++ Package.swift
@@ -9,10 +9,27 @@ let package = Package(
             targets: ["WolfCore"]),
         ],
     dependencies: [
+        .package(url: "https://github.com/wolfmcnally/WolfNesting", from: "2.0.0"),
+        .package(url: "https://github.com/wolfmcnally/WolfNumerics", from: "4.0.0"),
+        .package(url: "https://github.com/wolfmcnally/WolfOSBridge", from: "2.0.0"),
+        .package(url: "https://github.com/wolfmcnally/WolfPipe", from: "2.0.0"),
+        .package(url: "https://github.com/wolfmcnally/ExtensibleEnumeratedName", from: "2.0.0"),
+        .package(url: "https://github.com/wolfmcnally/WolfWith", from: "2.0.0"),
+        .package(url: "https://github.com/wolfmcnally/WolfStrings", from: "2.0.0"),
+        .package(url: "https://github.com/wolfmcnally/WolfFoundation", from: "3.0.0")
     ],
     targets: [
         .target(
             name: "WolfCore",
-            dependencies: [])
+            dependencies: [
+                "WolfNesting",
+                "WolfNumerics",
+                "WolfOSBridge",
+                "WolfPipe",
+                "ExtensibleEnumeratedName",
+                "WolfWith",
+                "WolfStrings",
+                "WolfFoundation"
+            ])
         ]
 )
