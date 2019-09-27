e9b670d5d3d3bb3de81401ec74f75e2dc22af4a7
Wolf McNally <wolf@wolfmcnally.com>
2019-09-27 15:15:52 -0700

    Added minimum platform versions.

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "WolfCore",
+    platforms: [
+        .iOS(.v9), .macOS(.v10_13), .tvOS(.v11)
+    ],
     products: [
         .library(
             name: "WolfCore",
